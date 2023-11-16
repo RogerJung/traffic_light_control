f4e378a92f4ad287a5ba487063b09e3bc2b3dcab
RogerJung <engineer54rj@gmail.com>
2023-11-16 23:03:23 +0800

    Add Zenoh queryable transmission.

--- controller.py
+++ controller.py
@@ -1,158 +1,113 @@
-import carla
-from pynput import keyboard
+#
+# Copyright (c) 2022 ZettaScale Technology
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+# which is available at https://www.apache.org/licenses/LICENSE-2.0.
+#
+# SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+#
+# Contributors:
+#   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+#
+
+import sys
 import time
-import zenoh
 import argparse
-
-spec_id = 0
-
-def on_press(k):
-    try:
-        pass
-    except AttributeError:
-        pass
-
-def on_release(k):
-
-    global traffic_lights
-    global spectator
-    global spec_id
-
-    global key
-
-    try:
-        print(' \r', end="")
-        if k.char == 'r':
-            
-            # Pub red signal to spectated light.
-            traffic_lights[spec_id + 30].set_state(carla.TrafficLightState.Red)
-
-            print("Set Traffic light to Red\n", end="")
-        elif k.char == 'g':
-
-            # Pub red signal to the other lights.
-            for i in range(30, 33):
-                if i != spec_id + 30:
-                    traffic_lights[i].set_state(carla.TrafficLightState.Red)
-            
-            # Pub green signal to spectated light.
-            traffic_lights[spec_id + 30].set_state(carla.TrafficLightState.Green)
-
-            print("Set Traffic light to Green\n", end="")
-        # elif key.char == '1':
-        #     print(spectator.get_transform())
-
-    except AttributeError:
-
-        # Using "TAB" to change perspective.
-        if k == keyboard.Key.tab:
-            spec_id += 1
-            if spec_id == 3:
-                spec_id = 0
-            if spec_id == 0:
-                spectator.set_transform(carla.Transform(
-                    carla.Location(x=83.918785, y=106.423500, z=6.480291), 
-                    carla.Rotation(pitch=-25.652645, yaw=73.435905, roll=-0.000030)))
-            elif spec_id == 1:
-                spectator.set_transform(carla.Transform(
-                    carla.Location(x=119.048515, y=133.816574, z=7.551385), 
-                    carla.Rotation(pitch=-23.294645, yaw=-172.816132, roll=-0.000031)))
-            elif spec_id == 2:
-                spectator.set_transform(carla.Transform(
-                    carla.Location(x=95.999725, y=157.935394, z=6.279342), 
-                    carla.Rotation(pitch=-19.706659, yaw=-110.011833, roll=-0.000030)))
-        elif k == keyboard.Key.esc:
-            # Stop listener
-            return False
-
+import json
+import zenoh
+from zenoh import config, QueryTarget
 
 # --- Command line argument parsing --- --- --- --- --- ---
 parser = argparse.ArgumentParser(
-    prog='z_pub',
-    description='zenoh pub traffic lights\' state')
-parser.add_argument('--key', '-k', dest='key',
-                    default='traffic_light',
+    prog='z_get',
+    description='zenoh get example')
+parser.add_argument('--mode', '-m', dest='mode',
+                    choices=['peer', 'client'],
                     type=str,
-                    help='The key expression to publish onto.')
-
+                    help='The zenoh session mode.')
+parser.add_argument('--connect', '-e', dest='connect',
+                    metavar='ENDPOINT',
+                    action='append',
+                    type=str,
+                    help='Endpoints to connect to.')
+parser.add_argument('--listen', '-l', dest='listen',
+                    metavar='ENDPOINT',
+                    action='append',
+                    type=str,
+                    help='Endpoints to listen on.')
+parser.add_argument('--target', '-t', dest='target',
+                    choices=['ALL', 'BEST_MATCHING', 'ALL_COMPLETE', 'NONE'],
+                    default='BEST_MATCHING',
+                    type=str,
+                    help='The target queryables of the query.')
+parser.add_argument('--value', '-v', dest='value',
+                    type=str,
+                    help='An optional value to send in the query.')
+parser.add_argument('--config', '-c', dest='config',
+                    metavar='FILE',
+                    type=str,
+                    help='A configuration file.')
+parser.add_argument('--command', '--cmd', dest='command',
+                    choices=['get', 'set'],
+                    required=True,
+                    type=str,
+                    help='The query command.')
+parser.add_argument('--light_id', '-i', dest='light_id',
+                    choices=[30, 31, 32],
+                    required=True,
+                    type=int,
+                    help='The traffic light\'s id')
+parser.add_argument('--state', '-s', dest='state',
+                    choices=['green', 'red'],
+                    type=str,
+                    help='The light\'s state')
 
 args = parser.parse_args()
-key = args.key
+conf = zenoh.Config.from_file(
+    args.config) if args.config is not None else zenoh.Config()
+if args.mode is not None:
+    conf.insert_json5(zenoh.config.MODE_KEY, json.dumps(args.mode))
+if args.connect is not None:
+    conf.insert_json5(zenoh.config.CONNECT_KEY, json.dumps(args.connect))
+if args.listen is not None:
+    conf.insert_json5(zenoh.config.LISTEN_KEY, json.dumps(args.listen))
 
-client = carla.Client('localhost', 2000)
-client.set_timeout(10.0)
-world = client.load_world('Town01')
+selector = 'traffic_light/' + str(args.light_id) + "/state"
 
-settings = world.get_settings()
-settings.synchronous_mode = True # Enables synchronous mode
-settings.fixed_delta_seconds = 0.05
-world.apply_settings(settings)
+target = {
+    'ALL': QueryTarget.ALL(),
+    'BEST_MATCHING': QueryTarget.BEST_MATCHING(),
+    'ALL_COMPLETE': QueryTarget.ALL_COMPLETE(),
+}.get(args.target)
 
-traffic_lights = world.get_actors().filter("traffic.traffic_light")
+# Zenoh code  --- --- --- --- --- --- --- --- --- --- ---
 
-spectator = world.get_spectator()
-spectator.set_transform(carla.Transform(
-    carla.Location(x=83.918785, y=106.423500, z=6.480291), 
-    carla.Rotation(pitch=-25.652645, yaw=73.435905, roll=-0.000030)))
+# initiate logging
+zenoh.init_logger()
+
+print("Opening session...")
+session = zenoh.open(conf)
+
+print("Sending Query '{}'...".format(selector))
+
+if args.command == 'get':
+    replies = session.get(selector, zenoh.Queue(), target=target, value=None)
+elif args.command == 'set' and args.state is not None:
+    replies = session.get(selector, zenoh.Queue(), target=target, value=args.state)
+else:
+    print('Non-valid query.')
+    replies = None
+
+if replies:
+    for reply in replies.receiver:
+        try:
+            print(">> Received ('{}': '{}')"
+                .format(reply.ok.key_expr, reply.ok.payload.decode("utf-8")))
+        except:
+            print(">> Received (ERROR: '{}')"
+                .format(reply.err.payload.decode("utf-8")))
 
 
-print("--------------------------------")
-print("| ESC : exit controller.       |")
-print("| TAB : change perspective.    |")
-print("|  r  : change light to red.   |")
-print("|  g  : change light to green. |")
-print("--------------------------------")
-
-
-for i in range(30, 33):
-    traffic_lights[i].set_red_time(1000.0)
-    traffic_lights[i].set_green_time(5.0)
-    traffic_lights[i].set_yellow_time(0.0)
-    traffic_lights[i].set_state(carla.TrafficLightState.Red)
-
-
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
-
-def main():
-    synchronous_master = True
-    try:
-
-        blueprint_library = world.get_blueprint_library()
-        ego_vehicle_bp = blueprint_library.find('vehicle.tesla.model3')
-        ego_vehicle_bp.set_attribute('color', '0, 0, 0')
-
-        transform = carla.Transform(carla.Location(x=87.991631, y=69.002403, z=1.0), 
-                                    carla.Rotation(pitch=12.049348, yaw=84.715179, roll=0.000388))
-        
-        # spawn npc vehicle
-        # ego_vehicle = world.spawn_actor(ego_vehicle_bp, transform)
-        # ego_vehicle.set_autopilot(True)
-
-        while listener.is_alive():
-            if synchronous_master:
-                # time.sleep(0.2)
-                world.tick()
-            else:
-                world.wait_for_tick()
-    finally:
-        # Turn off the sync. mode
-        if synchronous_master:
-            settings = world.get_settings()
-            settings.synchronous_mode = False
-            settings.fixed_delta_seconds = None
-            world.apply_settings(settings)
-        print('\rdestroying vehicles')
-        # ego_vehicle.destroy()
-        time.sleep(0.5)
-
-if __name__ == '__main__':
-
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print('done.')
+session.close()
--- main.py
+++ main.py
@@ -1,67 +1,202 @@
 import carla
 from pynput import keyboard
 import time
+import sys
+import argparse
+import json
+import zenoh
+from zenoh import config, Sample, Value
+from ast import literal_eval
 
+# --- Command line argument parsing --- --- --- --- --- ---
+parser = argparse.ArgumentParser(
+    prog='z_queryable',
+    description='zenoh queryable example')
+parser.add_argument('--mode', '-m', dest='mode',
+                    choices=['peer', 'client'],
+                    type=str,
+                    help='The zenoh session mode.')
+parser.add_argument('--connect', '-e', dest='connect',
+                    metavar='ENDPOINT',
+                    action='append',
+                    type=str,
+                    help='Endpoints to connect to.')
+parser.add_argument('--listen', '-l', dest='listen',
+                    metavar='ENDPOINT',
+                    action='append',
+                    type=str,
+                    help='Endpoints to listen on.')
+parser.add_argument('--key', '-k', dest='key',
+                    default='traffic_light/**',
+                    type=str,
+                    help='The key expression matching queries to reply to.')
+parser.add_argument('--value', '-v', dest='value',
+                    default='ACK',
+                    type=str,
+                    help='The value to reply to queries.')
+parser.add_argument('--complete', dest='complete',
+                    default=False,
+                    action='store_true',
+                    help='Declare the queryable as complete w.r.t. the key expression.')
+parser.add_argument('--config', '-c', dest='config',
+                    metavar='FILE',
+                    type=str,
+                    help='A configuration file.')
+
+args = parser.parse_args()
+conf = zenoh.Config.from_file(args.config) if args.config is not None else zenoh.Config()
+if args.mode is not None:
+    conf.insert_json5(zenoh.config.MODE_KEY, json.dumps(args.mode))
+if args.connect is not None:
+    conf.insert_json5(zenoh.config.CONNECT_KEY, json.dumps(args.connect))
+if args.listen is not None:
+    conf.insert_json5(zenoh.config.LISTEN_KEY, json.dumps(args.listen))
+key = args.key
+value = args.value
+complete = args.complete
+
+def set_state(selector, new_state):
+    global traffic_lights
+    index = int(str(selector).split('/')[1])
+
+    if new_state == 'green':
+        for i in range(30, 33):
+            if i != index:
+                traffic_lights[i].set_state(carla.TrafficLightState.Red)
+        traffic_lights[index].set_state(carla.TrafficLightState.Green)
+    elif new_state == 'red':
+        traffic_lights[index].set_state(carla.TrafficLightState.Red)
+
+def get_state(selector):
+    global traffic_lights
+
+    index = str(selector).split('/')[1]
+    state = traffic_lights[int(index)].get_state()
+
+    return state
+    
+
+def queryable_callback(query):
+
+    global traffic_lights
+
+    print(f">> [Queryable ] Received Query '{query.selector}'" + (f" with value: {query.value.payload}" if query.value is not None else ""))
+    # query.reply(Sample(key, value))
+
+    if query.value is None:
+        # Get traffic light state
+        state = get_state(query.selector)
+        query.reply(Sample(str(query.selector), state))
+    else:
+        new_state = query.value.payload.decode('utf-8')
+        set_state(query.selector, new_state)
+
+spec_id = 0
+
+def on_press(k):
+    try:
+        pass
+    except AttributeError:
+        pass
+
+def on_release(k):
+    global spectator
+    global spec_id
+
+    if k == keyboard.Key.tab:
+        spec_id += 1
+        if spec_id == 3:
+            spec_id = 0
+        if spec_id == 0:
+            spectator.set_transform(carla.Transform(
+                carla.Location(x=83.918785, y=106.423500, z=6.480291), 
+                carla.Rotation(pitch=-25.652645, yaw=73.435905, roll=-0.000030)))
+        elif spec_id == 1:
+            spectator.set_transform(carla.Transform(
+                carla.Location(x=119.048515, y=133.816574, z=7.551385), 
+                carla.Rotation(pitch=-23.294645, yaw=-172.816132, roll=-0.000031)))
+        elif spec_id == 2:
+            spectator.set_transform(carla.Transform(
+                carla.Location(x=95.999725, y=157.935394, z=6.279342), 
+                carla.Rotation(pitch=-19.706659, yaw=-110.011833, roll=-0.000030)))
+    elif k == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+
+# create a client in the Carla simulator
 client = carla.Client('localhost', 2000)
 client.set_timeout(10.0)
-world = client.get_world()
+world = client.load_world('Town01')
 
 settings = world.get_settings()
 settings.synchronous_mode = True # Enables synchronous mode
 settings.fixed_delta_seconds = 0.05
 world.apply_settings(settings)
 
-light_manager = world.get_lightmanager()
-lights = light_manager.get_all_lights()
-
-traffic_lights = []
-
-tmp_map = world.get_map()
-for landmark in tmp_map.get_all_landmarks_of_type('1000001'):
-    traffic_light = world.get_traffic_light(landmark)
-    if traffic_light:
-        traffic_lights.append(traffic_light)
-
-light_id = 10
+# Get traffic lights
+traffic_lights = world.get_actors().filter("traffic.traffic_light")
 
 spectator = world.get_spectator()
-spectator.set_transform(carla.Transform(traffic_lights[light_id].get_location() + carla.Location(x=10) + carla.Location(y=7) + carla.Location(z=6),
-carla.Rotation(pitch=-20, yaw = 180)))
+spectator.set_transform(carla.Transform(
+    carla.Location(x=83.918785, y=106.423500, z=6.480291), 
+    carla.Rotation(pitch=-25.652645, yaw=73.435905, roll=-0.000030)))
 
-print("------------------------------")
-print("| r : change light to red.   |")
-print("| g : change light to green. |")
-print("------------------------------")
 
-# for i in traffic_lights:
-#     i.set_red_time(5.0)
-#     i.set_green_time(5.0)
-#     i.set_yellow_time(2.0)
-    
-# traffic_lights[light_id].set_red_time(5.0)
-traffic_lights[light_id].set_green_time(5.0)
-traffic_lights[light_id].set_yellow_time(2.0)
+for i in range(30, 33):
+    traffic_lights[i].set_red_time(1000.0)
+    traffic_lights[i].set_green_time(5.0)
+    traffic_lights[i].set_yellow_time(0.0)
+    traffic_lights[i].set_state(carla.TrafficLightState.Red)
 
-while True:
+# initiate logging
+zenoh.init_logger()
 
-    with keyboard.Events() as events:
-        # Block at most one second
-        event = events.get(0.05)
-        if event is None:
-            pass
-        else:
-            try:
-                if type(event) == keyboard.Events.Release:
-                    print(' \r', end="")
-                    if event.key.char == 'r':
-                        if traffic_lights[light_id].get_state() == carla.TrafficLightState.Green:
-                            traffic_lights[light_id].set_state(carla.TrafficLightState.Red)
-                        print("Set Traffic light to Red\n", end="")
-                    elif event.key.char == 'g':
-                        if traffic_lights[light_id].get_state() == carla.TrafficLightState.Red:
-                            traffic_lights[light_id].set_state(carla.TrafficLightState.Green)
-                        print("Set Traffic light to Green\n", end="")
-            except AttributeError:
-                pass
+print("Opening session...")
+session = zenoh.open(conf)
 
-    world.tick()
+print("Declaring Queryable on '{}'...".format(key))
+queryable = session.declare_queryable(key, queryable_callback, complete)
+
+# keyboard control listener
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
+
+print("--------------------------------")
+print("| ESC : exit controller.       |")
+print("| TAB : change perspective.    |")
+print("--------------------------------")
+
+def main(args):
+    sync = True
+    try:
+        while listener.is_alive():
+            if sync:
+                world.tick()
+            else:
+                world.wait_for_tick()
+    finally:
+        if sync:
+            settings = world.get_settings()
+            settings.synchronous_mode = False
+            settings.fixed_delta_seconds = None
+            world.apply_settings(settings)
+        
+        queryable.undeclare()
+        session.close()
+        print('\rdestroying vehicles')
+        # ego_vehicle.destroy()
+        time.sleep(0.5)
+
+
+
+if __name__ == '__main__':
+
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print('done.')
